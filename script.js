29d959a78fef49997b0e28e640bc70ff699c4843
Daphne <daphne_bohm@hotmail.com>
2020-11-17 17:02:02 +0100

    changed header

--- script.js
+++ script.js
@@ -1,60 +1,90 @@
-$(document).ready(function() {
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const header = document.getElementById('header');
+    const screenWidth = document.documentElement.clientWidth;
     
+    const message = document.getElementById('message-icon');
+    const messageBox = document.getElementById('message-box');
+    const closeMenu = document.getElementById('close-menu');
+
+    const menu = document.getElementById('menu-icon');
+    const menuItems = document.getElementById('menu-items');
+
+    const search = document.getElementById('search-icon');
+    const searchItems = document.getElementById('search-items');
+    const closeSearch = document.getElementById('close-search-bar');
+    const hiddenSearchIcon = document.getElementById('search-icon-hidden');
+
+    const bag = document.getElementById('bag-icon');
+    const shoppingBox = document.getElementById('shopping-bag-box');
+    const closeShopping = document.getElementById('close-shopping-bag');
+
+
+    /****************************************HEADER****************************************/
+
     // MESSAGE
-    $('#message-icon').click(function(){
-        $('#message-icon').hide();
-        $('#menu').hide();
-        $('#search').hide();
-        $('#logo').hide();
-        $('#bag').hide();
-        $('#message-box').animate({width:'toggle'});
+    message.addEventListener('click', () =>{
+
+        if(screenWidth < 768){
+            messageBox.classList.add('showMessageSM');
+        }else{
+            messageBox.classList.add('showMessageMD');
+            header.classList.add('move680px');
+        }
+
     })
 
-    $('#close-menu').click(function(){
-        $('#message-box').hide('slow');
-        function showIcon(){
-            $('#message-icon').show();
-            $('#menu').show();
-            $('#search').show();
-            $('#logo').show();
-            $('#bag').show();
+    closeMenu.addEventListener('click', () =>{
+        if(screenWidth < 768){
+            messageBox.classList.remove('showMessageSM');
+        }else{
+            messageBox.classList.remove('showMessageMD');
+            header.classList.remove('move680px');
         }
-        setTimeout(showIcon, 450);
     })
+
 
     // MENU
-    $('.fa-bars').click(function(){
-        $('.menu-items').slideToggle();
+    menu.addEventListener('click', () => {
+        menuItems.classList.toggle('showMenuOrSearch');
     })
+
 
     // SEARCH
-    $('.fa-search').click(function(){
-        $('#search-items').slideToggle();
-    })
-
-    $('#close-search-bar').click(function(){
-        $('#search-items').slideUp();
-    })
-
-    // SHOPPING
-    $('#bag').click(function(){
-        $('#message-icon').hide();
-        $('#menu').hide();
-        $('#search').hide();
-        $('#logo').hide();
-        $('#shopping-bag-box').animate({width:'toggle'});
-    })
-
-    $('#close-shopping-bag').click(function(){
-        $('#shopping-bag-box').hide('slide');
-        function showIcon(){
-            $('#message-icon').show();
-            $('#menu').show();
-            $('#search').show();
-            $('#logo').show();
-            $('#bag').show();
+    search.addEventListener('click', () => {
+        if(screenWidth < 768){
+            searchItems.classList.toggle('showMenuOrSearch');
+        }else{
+            searchItems.classList.toggle('showMenuOrSearch');
+            searchItems.classList.contains('showMenuOrSearch') ? hiddenSearchIcon.classList.add('showLoop') : hiddenSearchIcon.classList.remove('showLoop');
         }
-        setTimeout(showIcon, 450);
     })
+
+    closeSearch.addEventListener('click', () => {
+        if(searchItems.classList.contains('showMenuOrSearch')) searchItems.classList.remove('showMenuOrSearch');
+    })
+
+
+    // BAG
+    bag.addEventListener('click', () => {
+        if(screenWidth < 768){
+            shoppingBox.classList.add('showBagSM');
+        }else{
+            shoppingBox.classList.add('showBagMD');
+            header.classList.add('goBack680px');
+        }
+    })
+
+    closeShopping.addEventListener('click', (e) => {
+        if(screenWidth < 768){
+            shoppingBox.classList.remove('showBagSM');
+        }else{
+            shoppingBox.classList.remove('showBagMD');
+            header.classList.remove('goBack680px');
+        }
+    })
+
+
 
 });
